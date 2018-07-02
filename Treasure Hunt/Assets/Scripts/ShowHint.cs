68810866640bb4351083ed90341177cb49fd7540
DESKTOP-GV05K4B\ekadewi <ekadewinur034@gmail.com>
2018-07-02 10:49:55 +0700

    TH finish for now

--- Treasure Hunt/Assets/Scripts/ShowHint.cs
+++ Treasure Hunt/Assets/Scripts/ShowHint.cs
@@ -4,13 +4,29 @@ using UnityEngine;
 
 public class ShowHint : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    public GameObject panel;
+    private float timeLeft;
+
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        timeLeft -= Time.deltaTime;
+        if (timeLeft < 0)
+        {
+            panel.SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "hint")
+        {
+            panel.SetActive(true);
+            timeLeft = 5;
+        }
+    }
 }
---
+++ Treasure Hunt/Assets/Scripts/showBantuan.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class showBantuan : MonoBehaviour {
+
+    public GameObject panel;
+    private float timeLeft;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        if (timeLeft < 0)
+        {
+            panel.SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "hint")
+        {
+            panel.SetActive(true);
+            timeLeft = 3;
+        }
+    }
+}
